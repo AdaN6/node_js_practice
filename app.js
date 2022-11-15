11010248596a731042082e762dd7508bc074358d
AdaN6 <a_disney_da@hotmail.com>
2022-11-15 17:08:47 +0100

    created html file, insert rs.readfile in nodejs

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-const http = require('http')
-const port = 3004
+const http = require("http");
+const fs = require("fs");
+const port = 3004;
 
-const server = http.createServer(function(req, res) {
-res.write('Hello Node')
-res.end()
-})
-
-server.listen(port, function(error){
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("index.html", function (error, data) {
     if (error) {
-        console.log('something went wrong', error)
+      res.writeHead(404);
+      res.write("Error: File not found");
     } else {
-        console.log('Server is listening on port ' + port)
+      res.write(data);
     }
-})
+    res.end();
+  });
+});
+
+server.listen(port, function (error) {
+  if (error) {
+    console.log("something went wrong", error);
+  } else {
+    console.log("Server is listening on port " + port);
+  }
+});
